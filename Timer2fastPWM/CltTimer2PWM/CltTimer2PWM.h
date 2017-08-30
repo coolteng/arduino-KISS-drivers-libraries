f59f7fd58c43a108836c40d833855f88d4186e4d
鄧崇林(Chung-Lin Teng) <coolteng@gmail.com>
2017-08-30 16:06:11 +0800

    Create CltTimer2PWM.h

---
+++ Timer2fastPWM/CltTimer2PWM/CltTimer2PWM.h
@@ -0,0 +1,19 @@
+// 作者：鄧崇林
+#ifndef CltTimer2PWM_h
+#define CltTimer2PWM_h
+
+#include "Arduino.h"
+
+class CltTimer2PWM
+{
+  public:
+    void initMode3(int pwmPin);
+	void putPgm2Sound(int soundPin, const unsigned char *cltPointer);
+	void putByte2Sound(int soundPin, byte sampleByte);
+    void noClkSource();
+};
+
+// 沒有下一行會引發編譯錯誤: 'Timer2PWM' was not declared in this scope
+extern CltTimer2PWM Timer2PWM;
+
+#endif
